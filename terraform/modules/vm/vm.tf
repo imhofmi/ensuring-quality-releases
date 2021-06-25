7aeb950c8eda75a29b35cc215b45ea787ed5830a
Michael Imhof <michael.i.imhof@daimler.com>
2021-06-25 14:19:09 +0200

    Added public key.

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -18,10 +18,10 @@ resource "azurerm_linux_virtual_machine" "test" {
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   network_interface_ids = [azurerm_network_interface.test.id,]
-  # admin_ssh_key {
-  #   username   = "azureuser"
-  #   public_key = file("./id_rsa.pub") #"file("~/.ssh/id_rsa.pub")"
-  # }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4p3GI6WS8jJuooTN5tyawTAzaL+OfnEqgq32HgmIjUc945TGsX8C3s+wvPwr1WU250te2vhCuuBQFQ9i/gNSL1w2dqGIF++upomUfkljT/rPid1Qw+eVBFvgkapHPhaCVhornFmZhliVrtMboVmXnhc0msnAeYN9lLOBm0Ch4UYweQMN+mh88CSKwtXldHo9WQAMqc04fbmjdclzA++BTxpOlM+mhhCAYF5fqW3c5fbeWmYHxm+Ds/G0jkxUk1ycGX+tiXFrt0C5OHGRwU1fH7G4dn7e4mHiFan2iY41BnQD26eKt5mONN30Zn4kE3s49KIfBVJMIFB3SPj+iAFPLVeaPzvExQsLUM7laft6g67L+eoJnaa8E6SavBMX2EvusDR+Lft5F5gJPTUuOQ/OkzZB6AVhOzyK6XuQk1ybIaLzLVFEe4/kS920ADPKjaRHDGekictAKbvg8sW943YjY4LGMyfAZ3By4EjGTfVNvhfX7Dh90uVbqaafUEiUdFGTtjHXoIcnplXnPGHVDMfvYJN6HLx/cTdxn+dIyzMtgN5CyOzF15MzsNRoykjOBDy/tpAFIXq473d/BA99VEFK3rhfPbbvWf5CJM+189OP/g2RZqxcxENQ8pzkLK5z/gNdGrP+p3RwMA/01kIPhWMKdVLe7ruItdlxUnRFT5RynbQ== IMHOFMI@CMTCL060485" #file("./id_rsa.pub") #"file("~/.ssh/id_rsa.pub")"
+  }
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
