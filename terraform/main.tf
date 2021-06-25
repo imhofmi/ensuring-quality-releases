ead2cd408d04024ab7447ea59e76214fe092aba6
Michael Imhof <michael.i.imhof@daimler.com>
2021-06-25 09:27:17 +0200

    Updated terraform files.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
   features {}
 }
 terraform {
@@ -14,12 +10,12 @@ terraform {
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
+  source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
 module "network" {
-  source               = "../../modules/network"
+  source               = "./modules/network"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   virtual_network_name = "${var.virtual_network_name}"
@@ -30,7 +26,7 @@ module "network" {
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
+  source           = "./modules/networksecuritygroup"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
@@ -39,16 +35,25 @@ module "nsg-test" {
   address_prefix_test = "${var.address_prefix_test}"
 }
 module "appservice" {
-  source           = "../../modules/appservice"
+  source           = "./modules/appservice"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
   resource_group   = "${module.resource_group.resource_group_name}"
 }
 module "publicip" {
-  source           = "../../modules/publicip"
+  source           = "./modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
   resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "vm" {
+  source           = "./modules/vm"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "vm"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  public_ip        = "${module.publicip.public_ip_address_id}"
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,15 +1,9 @@
-# Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
-
 # Resource Group/Location
-location = ""
-resource_group = ""
-application_type = ""
+location = "West Europe"
+resource_group = "eqr-rg"
+application_type = "eqr-AppService"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "eqr-vnet"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
